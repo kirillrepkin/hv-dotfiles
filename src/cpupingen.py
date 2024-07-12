e6acc9be7ccefe51a339eeb9e877296c63b5321c
Kirill Repkin <krepkin@it-one.ru>
2024-07-12 17:34:08 +0300

    libvirt cpu pinning script

--- src/cpupingen.py
+++ src/cpupingen.py
@@ -23,6 +23,15 @@ class CorePinning:
 
     def system(self):
         self.type = [CoreType.system]
+    
+    def virtual(self):
+        self.type = [CoreType.virtual]
+    
+    def io(self):
+        self.type.append(CoreType.io)
+    
+    def idle(self):
+        self.type = [CoreType.idle]
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='CPU Pinning script')
@@ -30,17 +39,52 @@ if __name__ == "__main__":
     parser.add_argument('--threads-per-core', type=int, required=True, help='Thread per core')
     parser.add_argument('--io-threads', type=int, required=True, help='Count of emulator pins')
     parser.add_argument('--system-cores', type=str, required=True, help='List of system cores')
+    parser.add_argument('--virtual-cores', type=int, required=True, help='Count of virtual cores')
+
+    def is_io(pin: CorePinning):
+        return CoreType.io in pin.type
+    
+    def is_virtual(pin: CorePinning):
+        return CoreType.virtual in pin.type
+    
+    def is_system(pin: CorePinning):
+        return CoreType.system in pin.type
+
+    def is_idle(pin: CorePinning):
+        return CoreType.idle in pin.type
     
     args = parser.parse_args()
     args.system_cores = args.system_cores.split(',')
 
     pinnings = []
+    virt_cnt = 0
+    io_thread_cnt = 0
 
+    # default pin state
     for i in range(0, args.total_cores):
-        pinnings.append(CorePinning(i, CoreType.idle))
-
-    for pin in pinnings:
+        pin = CorePinning(i, CoreType.idle)
         if(str(pin.num) in args.system_cores):
             pin.system()
-    
-    print(pinnings)
+            pin.io()
+
+        else:
+            if virt_cnt < args.virtual_cores:
+                pin.virtual()
+                virt_cnt += 1
+        pinnings.append(pin)
+
+    print(f"<cpu mode='host-passthrough' check='none' migratable='on'>")
+    print(f"\t<topology sockets='1' dies='1' clusters='1' cores='{int(args.virtual_cores/args.threads_per_core)}' threads='{args.threads_per_core}'/>")
+    print(f"</cpu>")
+    print(f"<iothreads>{args.io_threads}</iothreads>")
+    print(f"<cputune>")
+    i=0
+    for pin in filter(is_virtual, pinnings):
+        print(f"\t<vcpupin vcpu='{i}' cpuset='{pin.num}'/>")
+        i += 1
+    print(f"\t<emulatorpin cpuset='{",".join(args.system_cores)}'/>")
+    i=1
+    for pin in range(0, args.io_threads):
+        print(f"\t<iothreadpin iothread='{i}' cpuset='{",".join(args.system_cores)}'/>")
+        i += 1
+    print(f"</cputune>")
