6cf2030024003df516c9f89ad394102675c63d38
Kirill Repkin <krepkin@it-one.ru>
2024-08-22 23:32:54 +0300

    udev rules

--- install.sh
+++ install.sh
@@ -15,3 +15,9 @@ update_symlink /etc/libvirt/hooks/qemu $(pwd)/libvirt/hook_qemu.sh
 # modprobe
 update_symlink /etc/modprobe.d/blacklist.conf $(pwd)/modprobe/blacklist.conf
 update_symlink /etc/modprobe.d/vfio.conf $(pwd)/modprobe/vfio.conf
+
+# udev rules
+update_symlink /etc/udev/rules.d/99-custom-hwmon.rules $(pwd)/udev/99-custom-hwmon.rules
+
+sudo udevadm control --reload-rules
+sudo udevadm trigger
